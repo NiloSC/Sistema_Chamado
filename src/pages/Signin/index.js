e1e0e3df10f9855dddc324cfe113cc014efd38fe
NiloSC <nilo.sc@hotmail.com>
2022-11-03 22:32:14 -0300

    Add-Paginas

---
+++ src/pages/Signin/index.js
@@ -0,0 +1,10 @@
+function Signin() {
+    return (
+        <div>
+            <h1>Pagina de Login</h1>
+        </div>
+    );
+}
+
+export default Signin; 
+
---
+++ src/services/firebaseConnections.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBftfC6PjKFPhJ2du11ODZlgLl2vB9cyYU",
+    authDomain: "chamado-31af6.firebaseapp.com",
+    projectId: "chamado-31af6",
+    storageBucket: "chamado-31af6.appspot.com",
+    messagingSenderId: "830233952660",
+    appId: "1:830233952660:web:7fc6d91017db9191334f81",
+    measurementId: "G-3ZYG3BRJGJ"
+  };
+  
+ if(!firebase.apss.length){
+    firebase.initializeApp(firebaseConfig);
+ } 
+
+
+ export default firebase;
