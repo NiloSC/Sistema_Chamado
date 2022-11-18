d972ef05ca402e093b9d94ebf0a47b8d6532f2e1
NiloSC <nilo.sc@hotmail.com>
2022-11-17 21:52:33 -0300

    Tela Signed

---
+++ src/contexts/auth.js
@@ -0,0 +1,21 @@
+import { useState, createContext, useEffect } from "react";
+import firebase from "../services/firebaseConnections";
+
+//criar o context 
+export const AuthContext = createContext({});
+
+
+function AuthProvider({ children }){
+    const[user, setUser] = useState({ id: 1, nome: 'Nilo' });
+    const [loadingAuth, setLoadingAuth] = useState(false);
+    
+    return(
+        <AuthContext.Provider value={{ signed: !!user, user }}>
+            {children}
+        </AuthContext.Provider>
+
+        
+    );
+}
+
+export default AuthProvider;
